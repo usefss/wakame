cf3a4c8229a70e8a03fa176038f4ef7970e3a89c
yosef slmln <flasnm@yahoo.com>
2020-05-13 18:18:41 +0430

    init atom plugin

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0 - First Release
+* yet learning how to catch time with editing code
+* this needs to connect to a postgress db on port 5432 for saving actions 
---
+++ spec/wakame-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WakameView from '../lib/wakame-view';
+
+describe('WakameView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
